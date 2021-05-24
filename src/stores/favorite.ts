22a46f4f278f2963cd0ff1912bf0bd01dacca738
KhalafAbdi <Abdifatahkhalaf@gmail.com>
2021-05-25 00:42:19 +0200

    using async storage

--- src/stores/favorite.ts
+++ src/stores/favorite.ts
@@ -1,23 +1,44 @@
 import {action, makeObservable, observable} from 'mobx'
 import {Repository} from '../types'
 
+import {storeRepository, getStoredRepositories} from '../util/asyncStorage'
+
 export class FavoritesStoreImpl {
   favorites: Repository[] = []
 
   constructor() {
     makeObservable(this, {
       favorites: observable,
+      load: action,
       add: action,
       remove: action,
+      updateStorage: action,
     })
+
+    this.load()
+  }
+
+  async updateStorage() {
+    storeRepository('Favorites', this.favorites)
+    this.load()
+  }
+
+  async load() {
+    const storedRepositories = await getStoredRepositories('Favorites')
+    if (storedRepositories) {
+      this.favorites = storedRepositories
+    }
   }
 
   add(item: Repository) {
     this.favorites.push(item)
+
+    this.updateStorage()
   }
 
   remove(id: number) {
     this.favorites = this.favorites.filter(item => item.id !== id)
+    this.updateStorage()
   }
 }
 
